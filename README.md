b6fcb7fa871f5131244078281381cfe449b504de
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2023-10-31 14:53:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RPG_Map_AdrianDorey
+ 
---
+++ RPG_Map_AdrianDorey/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Map_AdrianDorey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void DisplayMap()
+        {
+
+        }
+        static void DisplayMap(int scale)
+        {
+
+        }
+
+
+    }
+}
